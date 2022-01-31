4022682453da735f96f633ab97851d80e24bcabf
Ebenial <ebenial22@gmail.com>
2022-01-31 14:08:54 +0100

    premier commit

--- src/main.py
+++ src/main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pdftotext
+import pdf2txt
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Heil, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Frappe l\'horloge !')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Load your PDF
+with open("../Corpus_2022/Boudin-Torres-2006.pdf", "rb") as f:
+    pdf = pdftotext.PDF(f)
+
+# How many pages?
+print(len(pdf))
+
+# Iterate over all the pages
+for page in pdf:
+    print(page)
+
+# Read some individual pages
+print(pdf[0])
+print(pdf[1])
+
+# Read all the text into one string
+print("\n\n".join(pdf))
