95afa5305bc8330acbc870a11b156b4c925cda49
Pulkio <57195076+Pulkio@users.noreply.github.com>
2022-02-25 14:55:09 +0100

    Ajout début algorithme

--- src/main.py
+++ src/main.py
@@ -1,31 +1,76 @@
-import pdftotext
-import pdf2txt
+# import PyPDF2
+#
+# FILE_PATH = '../Corpus_2021/Das_Martins.pdf'
+# with open(FILE_PATH, mode='rb') as f:
+#     reader = PyPDF2.PdfFileReader(f)
+#     page = reader.getPage(0) #accessing to first page in file
+#     print(page.extractText())
+
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+import glob
+import os.path
+import os
+import re
+
+path = '../Corpus_2021/'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Heil, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def listdirectory(path):
+    fichier = []
+    l = glob.glob(path + '\\*')
+    for i in l:
+        if os.path.isdir(i):
+            fichier.extend(listdirectory(i))
+        else:
+            fichier.append(i)
+    return fichier
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Frappe l\'horloge !')
+pdfFiles = []
+pdfFiles = listdirectory(path)
+txtName = []
+for x in pdfFiles:
+    regex = re.search(".pdf$", x)
+    if (regex):
+        print(x)
+        txtName.append(".." + x.split(".")[2] + ".txt")
+
+print("\n")
+for y in txtName:
+    print(y)
 
 
-# Load your PDF
-with open("../Corpus_2022/Boudin-Torres-2006.pdf", "rb") as f:
-    pdf = pdftotext.PDF(f)
+#gs -o "output.pdf" -sDEVICE=pdfwrite input.pdf
+def convertPdfToTxt(path):
+    command = 'gs -o "' + path + "\" -sDEVICE=pdfwrite " + path
+    print(command + "bbbbbbbbbbbbbbbbbbbbbbbb")
+    os.system(command)
+    output_string = StringIO()
+    with open(path, 'rb') as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
+    return output_string
 
-# How many pages?
-print(len(pdf))
 
-# Iterate over all the pages
-for page in pdf:
-    print(page)
 
-# Read some individual pages
-print(pdf[0])
-print(pdf[1])
+for i in range(len(pdfFiles)) :
+     with open(txtName[i], 'w', encoding='utf-8') as f:
+         print(pdfFiles[i] + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+         # print(convertPdfToTxt(pdfFiles[i]).getvalue())
 
-# Read all the text into one string
-print("\n\n".join(pdf))
+         f.write(convertPdfToTxt(pdfFiles[i]).getvalue())
+
+
+# print(convertPdfToTxt(pdfFiles[0]).getvalue())
