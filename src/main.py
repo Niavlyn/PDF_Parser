22ca195c0b623bc2914083073bbb9e43bd7b8db2
Ebenial <ebenial22@gmail.com>
2022-02-26 17:22:55 +0100

    Récupération des fichiers pdf et conversion au format txt

--- src/main.py
+++ src/main.py
@@ -1,76 +1,66 @@
-# import PyPDF2
-#
-# FILE_PATH = '../Corpus_2021/Das_Martins.pdf'
-# with open(FILE_PATH, mode='rb') as f:
-#     reader = PyPDF2.PdfFileReader(f)
-#     page = reader.getPage(0) #accessing to first page in file
-#     print(page.extractText())
-
-from io import StringIO
-
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfparser import PDFParser
 import glob
 import os.path
 import os
 import re
+import shutil
 
-path = '../Corpus_2021/'
+path = '../Corpus_2022/'
+pathTxt = '../Corpus_2022_txt/'
 
 
 def listdirectory(path):
     fichier = []
-    l = glob.glob(path + '\\*')
+    l = glob.glob(path + '//*')
     for i in l:
         if os.path.isdir(i):
             fichier.extend(listdirectory(i))
         else:
+            new_name = i.replace(" ", "")
+            os.rename(i, new_name)
             fichier.append(i)
     return fichier
 
-
+allFiles = []
+allFiles = listdirectory(path)
+txtFiles = []
 pdfFiles = []
-pdfFiles = listdirectory(path)
-txtName = []
-for x in pdfFiles:
+
+for x in allFiles:
     regex = re.search(".pdf$", x)
-    if (regex):
-        print(x)
-        txtName.append(".." + x.split(".")[2] + ".txt")
+    if regex:
+        pdfFiles.append(x)
+        filename = ".." + x.split(".")[2] + ".txt"
+        txtFiles.append((pathTxt + filename.split("/")[2]))
 
-print("\n")
-for y in txtName:
-    print(y)
+    regexTxt = re.search(".txt$", x)
+    if regexTxt:
+        shutil.copyfile(x, (pathTxt + x.split("/")[2]))
 
 
-#gs -o "output.pdf" -sDEVICE=pdfwrite input.pdf
-def convertPdfToTxt(path):
-    command = 'gs -o "' + path + "\" -sDEVICE=pdfwrite " + path
-    print(command + "bbbbbbbbbbbbbbbbbbbbbbbb")
+
+def convertPdfToTxt(filePdf, fileTxt):
+    #si le fichier contient un espace, penser à ajouter un \ avant le caractère
+
+    # pdf2txt.py[-P password] [-o output][-t text | html | xml | tag] [-O output_dir][-c encoding] [-s scale][-R
+    # rotation] [-Y normal | loose | exact][-p pagenos] [-m maxpages] [-S][-C][-n][-A][-V] [-M char_margin][-L
+    # line_margin] [-W word_margin] [-F boxes_flow][-d] input.pdf...
+    command = 'pdf2txt.py -o ' + fileTxt + ' ' + filePdf
+
     os.system(command)
-    output_string = StringIO()
-    with open(path, 'rb') as in_file:
-        parser = PDFParser(in_file)
-        doc = PDFDocument(parser)
-        rsrcmgr = PDFResourceManager()
-        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        for page in PDFPage.create_pages(doc):
-            interpreter.process_page(page)
-    return output_string
+    # output_string = StringIO()
+    # with open(path, 'rb') as in_file:
+    #     parser = PDFParser(in_file)
+    #     doc = PDFDocument(parser)
+    #     rsrcmgr = PDFResourceManager()
+    #     device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    #     interpreter = PDFPageInterpreter(rsrcmgr, device)
+    #     for page in PDFPage.create_pages(doc):
+    #         interpreter.process_page(page)
+    # return output_string
 
 
-
-for i in range(len(pdfFiles)) :
-     with open(txtName[i], 'w', encoding='utf-8') as f:
-         print(pdfFiles[i] + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-         # print(convertPdfToTxt(pdfFiles[i]).getvalue())
-
-         f.write(convertPdfToTxt(pdfFiles[i]).getvalue())
-
-
-# print(convertPdfToTxt(pdfFiles[0]).getvalue())
+for i in range(len(txtFiles)) :
+     with open(txtFiles[i], 'w', encoding='utf-8') as f:
+         print(txtFiles[i] + " toto")
+         #print(pdfFiles[i] + " aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+         #convertPdfToTxt(pdfFiles[i], txtFiles[i])
