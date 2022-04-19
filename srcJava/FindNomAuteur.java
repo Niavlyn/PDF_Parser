3ec615989bf5c550ffb798b9557a228a57abc235
pulkio <guillaume.cobat@gmx.fr>
2022-04-19 16:47:00 +0200

    Corrction bug

--- srcJava/FindNomAuteur.java
+++ srcJava/FindNomAuteur.java
@@ -5,31 +5,33 @@ public class FindNomAuteur {
     public FindNomAuteur(Parser parser) {
         String authors ="";
         if(parser.getAuthorsTab()[0].equals("")) {
-            String[] emails = parser.getEmail().split(";");
-            String[] noms = new String[emails.length];
-            String[] prenoms = new String[emails.length];
-            String[] authorTab = new String[emails.length];
-            for(int i = 0; i < emails.length; i++) {
+            if(!parser.getEmail().equals("")) {
+                String[] emails = parser.getEmail().split(";");
+                String[] noms = new String[emails.length];
+                String[] prenoms = new String[emails.length];
+                String[] authorTab = new String[emails.length];
+                for(int i = 0; i < emails.length; i++) {
 //                System.out.println(emails[i]);
-                String nomPrenom = emails[i].split("@")[0];
+                    String nomPrenom = emails[i].split("@")[0];
 
-                //System.out.println("Prenom : " + nomPrenom.split("\\.")[0]);
-                prenoms[i] = nomPrenom.split("\\.")[0];
-                try {
-                    noms[i] = nomPrenom.split("\\.")[1];
-                } catch (Exception ignored){}
-                if(noms[i] != null) {
-                    authorTab[i] = prenoms[i] + " " + noms[i];
-                    //authors += prenoms[i] + " " + noms[i] + " ; ";
-                } else {
-                    //authors += prenoms[i]+ " ; ";
-                    authorTab[i] = prenoms[i];
+                    //System.out.println("Prenom : " + nomPrenom.split("\\.")[0]);
+                    prenoms[i] = nomPrenom.split("\\.")[0];
+                    try {
+                        noms[i] = nomPrenom.split("\\.")[1];
+                    } catch (Exception ignored){}
+                    if(noms[i] != null) {
+                        authorTab[i] = prenoms[i] + " " + noms[i];
+                        //authors += prenoms[i] + " " + noms[i] + " ; ";
+                    } else {
+                        //authors += prenoms[i]+ " ; ";
+                        authorTab[i] = prenoms[i];
+                    }
+
+
                 }
-
-
+                parser.setAuthorsTab(authorTab);
+                //parser.setAuthors(authors);
             }
-            parser.setAuthorsTab(authorTab);
-            //parser.setAuthors(authors);
         }
 
 
